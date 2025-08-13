b3157d3af7899567409ffe200f716bcd36bbc8ac
shanchao90 <shanchao90@163.com>
2025-08-13 17:14:12 +0800

    gx

--- tools/log.c
+++ tools/log.c
@@ -1,27 +1,4 @@
-#include<stdio.h>
-#include<time.h>
-
-// 日志级别
-typedef enum{
-    LOG_INFO, //信息
-    LOG_WARN, //警告
-    LOG_ERROR //错误
-}LogLevel;
-
-// 日志输出目标
-typedef enum{
-    LOG_TO_CONSOLE, //控制台
-    LOG_TO_FILE, //警告
-    LOG_TO_BOTH //两者都输出
-}LogTarget;
-
-// 日志结构体
-typedef struct {
-    FILE* log_file;
-    char file_path[256];
-    LogTarget target;
-    LogLevel min_level;
-}Logger;
+#include "log.h"
 
 //初始化日志器
 Logger* logger_init(const char* file_path,LogTarget target,LogLevel min_level){
